572f8497f8a9f1bca12ec18784baedb601411da0
Demetris Perdikos <47956627+demetrisperdikos@users.noreply.github.com>
2023-09-28 17:11:45 -0400

    Update App.js

--- App.js
+++ App.js
@@ -18,14 +18,7 @@ if (!firebase.apps.length) {
   // Initialize Firebase
   firebase.initializeApp({
     // Your firebase configuration object
-    apiKey: "AIzaSyAu9LaK2L4WFlq1JVesz5XTwwS82CXsjUw",
-    authDomain: "playhobokenapp.firebaseapp.com",
-    databaseURL: "https://playhobokenapp-default-rtdb.firebaseio.com",
-    projectId: "playhobokenapp",
-    storageBucket: "playhobokenapp.appspot.com",
-    messagingSenderId: "800030455108",
-    appId: "1:800030455108:web:060fa8ecd5f2b266e99311",
-    measurementId: "G-5E91HVBDBL"
+   
   });
 }
 
